62ebbd7d977837ddc4f9022603f4811cb565f9d3
Ilya <Luzich2018@mail.ru>
2025-02-27 20:29:42 +0300

    Описание изменений

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,7 +11,6 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
-        Util util = new Util();
-        util.getConnection();
+        Util.getConnection();
     }
 }
